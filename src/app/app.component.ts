821419ab315a5166d3ba7c8fbd17870f0206eb29
ParthipanSrinivasan <parthipanks2001@gmail.com>
2023-04-13 16:00:37 +0530

    json angular table formate click button angularforms used'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HttpService } from './http.service';
 import { NgClass } from '@angular/common';
 import { NgStyle } from '@angular/common';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -12,16 +13,22 @@ import { NgStyle } from '@angular/common';
 })
 
 export class AppComponent{
-  nam: any[]=['anil','dis','assd','hadfjk'];
+ // nam: any[]=['anil','dis','assd','hadfjk'];
   value: any=[];
-  show =true;
-  @Input()
-  ngSwitch: any
-  @Input()
-  ele = 'name'
-  @Input()
-  blue= 'yellow'
-  constructor(private httpService: HttpService) {
+ // name ='';
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+  });
+   myClickFunction(value:any) { 
+   console.log(value);
+  // this.name+=value;
+  this.profileForm?.get('firstName')?.setValue(value);
+}
+onSubmit() {
+  console.log(this.profileForm.value);
+ 
+}
+constructor(private httpService: HttpService, fb: FormBuilder) {
     this.httpService.sendGetRequest().subscribe((data:any) => {
         this.value=data;
         console.log(this.value);
