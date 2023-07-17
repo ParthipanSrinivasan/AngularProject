fd2aa82fe20f829f7d46229b176dcb2b4e2ede27
ParthipanSrinivasan <parthipanks2001@gmail.com>
2023-07-17 17:31:08 +0530

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,10 @@
-import { Component,OnInit } from '@angular/core';
-import { FormControl,FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-/* export class AppComponent {
-  formdata:any = {};
-
-  submit(){
-    console.log(this.formdata);
-  }
-} */
-export class AppComponent implements OnInit {
-  userName:any;
-  sureName:any;
-  emailId:any;
-  newPassword:any;
-  date:any;
-  formdata:any;
-  ngOnInit() {
-    this.formdata = new FormGroup({ 
-      userName: new FormControl(),
-      sureName: new FormControl(),
-      emailId: new FormControl(),
-      newPassword: new FormControl(),
-      date: new FormControl()
-   }); 
-  }
-  onClickSubmit(data:any) {
-    this.userName = data.userName;
-    this.sureName = data.sureName;
-    this.emailId = data.emailId;
-    this.newPassword = data.newPassword;
-    this.date = data.date;
-    console.log(this.userName);
-    console.log(this.sureName);
-    console.log(this.emailId);
-    console.log(this.newPassword);
-    console.log(this.date);
-  }
+export class AppComponent {
+  title = 'material';
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ServerComponent } from './server/server.component';
-import { HomeComponent } from './home/home.component';
-import { UserComponent } from './user/user.component';
-import { AboutComponent } from './about/about.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    HomeComponent,
-    UserComponent,
-    AboutComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
     FormsModule,
-    ReactiveFormsModule,
-    AppRoutingModule
+    MatSlideToggleModule,
+    MatDividerModule, MatListModule
   ],
-  providers: [], 
+  providers: [],
   bootstrap: [AppComponent]
-})   
-export class AppModule { 
-  
-}
+})
+export class AppModule { }
